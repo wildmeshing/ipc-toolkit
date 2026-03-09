5ca173a3fd5f64345babf7b6d1a04f783d4b292b
federico <f.sichetti@gmail.com>
2026-03-09 19:17:58 -0400

    respect skip_obstacles setting

--- src/ipc/high_order_contact/high_order_collisions_builder.cpp
+++ src/ipc/high_order_contact/high_order_collisions_builder.cpp
@@ -315,9 +315,8 @@ void QuadratureCollisionsBuilder::build_vertex_collisions(
     const CollisionMesh& mesh = point_potential->mesh;
     for (size_t i = start_i; i < end_i; i++) {
         const index_t vi = vertex_indices[i];
-        if (mesh.is_obstacle_vertex(vi)) {
-            continue;
-        }
+        if (point_potential->params.skip_obstacle
+            && mesh.is_obstacle_vertex(vi)) continue;
         vertex_collisions.emplace_back(
             vi, point_potential->build_collisions_at_vertex(vertices, vi));
     }
@@ -332,9 +331,8 @@ void QuadratureCollisionsBuilder::build_face_collisions(
     const CollisionMesh& mesh = point_potential->mesh;
     for (size_t i = start_i; i < end_i; i++) {
         const index_t fi = face_indices[i];
-        if (mesh.is_obstacle_face(fi)) {
-            continue;
-        }
+        if (point_potential->params.skip_obstacle
+            && mesh.is_obstacle_face(fi)) continue;
         face_collisions.emplace_back(
             fi, point_potential->build_collisions_at_face_center(vertices, fi));
     }
@@ -381,18 +379,18 @@ void QuadratureCollisionsBuilder::build_edge_edge_collisions(
             continue;
         }
 
-        if (dtype == EdgeEdgeDistanceType::EA_EB || dtype == EdgeEdgeDistanceType::EA_EB0
-            || dtype == EdgeEdgeDistanceType::EA_EB1) {
-            if (!mesh.is_obstacle_edge(ei)) {
-                edge_edge_collisions.emplace_back(std::make_pair(ei, ej), point_potential->build_collisions_at_edge_edge_closest_point(vertices, ei, ej));
-            }
+        if (!(params.skip_obstacle && mesh.is_obstacle_edge(ei))
+            && (dtype == EdgeEdgeDistanceType::EA_EB
+                || dtype == EdgeEdgeDistanceType::EA_EB0
+                || dtype == EdgeEdgeDistanceType::EA_EB1)) {
+            edge_edge_collisions.emplace_back(std::make_pair(ei, ej), point_potential->build_collisions_at_edge_edge_closest_point(vertices, ei, ej));
         }
 
-        if (dtype == EdgeEdgeDistanceType::EA_EB || dtype == EdgeEdgeDistanceType::EA0_EB
-            || dtype == EdgeEdgeDistanceType::EA1_EB) {
-            if (!mesh.is_obstacle_edge(ej)) {
-                edge_edge_collisions.emplace_back(std::make_pair(ej, ei), point_potential->build_collisions_at_edge_edge_closest_point(vertices, ej, ei));
-            }
+        if (!(params.skip_obstacle && mesh.is_obstacle_edge(ej))
+            && (dtype == EdgeEdgeDistanceType::EA_EB
+                || dtype == EdgeEdgeDistanceType::EA0_EB
+                || dtype == EdgeEdgeDistanceType::EA1_EB)) {
+            edge_edge_collisions.emplace_back(std::make_pair(ej, ei), point_potential->build_collisions_at_edge_edge_closest_point(vertices, ej, ei));
         }
     }
 }
